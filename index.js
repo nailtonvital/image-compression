687a9246d9e644869b1c63ea1df97de8ec40396f
Nailton Vital <nailtonvital35@gmail.com>
2022-08-08 22:10:12 -0300

    cloudinary

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const express = require("express");
 const multer = require("multer");
 const sharp = require("sharp");
-const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
+const { Readable } = require("stream");
+
 
 const app = express();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
+cloudinary.config({
+    cloud_name: "YOUR_CLOUD_NAME",
+    api_key: "YOUR_API_KEY",
+    api_secret: "YOUR_API_SECRET",
+});
+
+const bufferToStream = (buffer) => {
+    const readable = new Readable({
+        read() {
+            this.push(buffer);
+            this.push(null);
+        },
+    });
+    return readable;
+}
+
 app.use(express.json())
 app.use(express.static("./uploads"));
 sharp.cache(false);
+
 
 app.get("/", (req, res) => {
     return res.json({ message: "Hello world 🔥🇵🇹" });
 });
 
 app.post("/upload", upload.single("picture"), async (req, res) => {
-    fs.access("./uploads", (error) => {
-        if (error) {
-            fs.mkdirSync("./uploads");
+    const data = await sharp(req.file.buffer).webp({ quality: 20 }).toBuffer();
+    const stream = cloudinary.uploader.upload_stream(
+        { folder: "DEV" },
+        (error, result) => {
+            if (error) return console.error(error);
+            return res.json({ URL: result.secure_url });
         }
-    });
-    const { buffer, originalname } = req.file;
-    const timestamp = new Date().toISOString();
-    const ref = `${originalname}.webp`;
-    await sharp(buffer)
-        .webp({ quality: 20 })
-        .toFile("./uploads/" + ref);
-    const link = `http://localhost:3000/${ref}`;
-    return res.json({ link });
+    );
+    bufferToStream(data).pipe(stream);
 });
 
 app.listen(3000);
